64a8ca3cc0f9041a1e7ff66544d02c21ced9ab60
NikolayVatan <51362503+NikolayVatan@users.noreply.github.com>
2019-10-15 15:05:31 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,2 @@
+USER = 'ivan.petrov.lol@yandex.ru'
+PASSWORD = 'P@ssw0rd'
---
+++ originacc.py
@@ -0,0 +1,48 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import os
+import time
+from config import *
+
+
+def origin_login(email):
+    driver = webdriver.Chrome(executable_path=f'{os.getcwd()}/chromedriver')
+    driver.set_window_size(1280, 768)
+    wait = WebDriverWait(driver, 15)
+    driver.get('https://www.origin.com')
+    wait.until(EC.visibility_of_element_located((By.XPATH, '//div[text()="Войти"]')))
+    driver.find_element_by_xpath('//div[text()="Войти"]').click()
+    login_window = driver.window_handles[1]
+    driver.switch_to.window(login_window)
+    wait.until(EC.visibility_of_element_located((By.XPATH, '//input[@id="email"]')))
+    driver.find_element_by_xpath('//input[@id="email"]').send_keys(USER)
+    driver.find_element_by_xpath('//input[@id="password"]').send_keys(PASSWORD)
+    driver.find_element_by_xpath('//*[@id="content"]').click()
+    driver.find_element_by_xpath('//a[@id="logInBtn"]').click()
+    driver.switch_to.window(driver.window_handles[0])
+    wait.until(EC.invisibility_of_element_located((By.XPATH, '//div[text()="Sign In"]')))
+    driver.find_element_by_xpath('//input[@ng-model="searchString"]').send_keys(email)
+    # error = driver.find_element_by_xpath('//div[@class="origin-search-results
+    # l-origin-section"]/div/origin-search-no-results/section/h1/span') print(error.text)
+    wait = WebDriverWait(driver, 15)
+    try:
+        wait.until(EC.visibility_of_element_located((By.XPATH, '//a[@class="otka otkc origin-friendtile-originId"]')))
+        time.sleep(.5)
+        driver.find_element_by_xpath('//a[@class="otka otkc origin-friendtile-originId"]/strong').click()
+        time.sleep(5)
+        nikcname = driver.find_element_by_xpath('//div[@class="otktitle-page origin-profile-header-username"]')
+        img = driver.find_element_by_xpath('//div[@class="origin-profile-header-info-section"]/a/img')
+        total = {
+            'photo': img.get_attribute('src'), 'name': nikcname.text, 'url': driver.current_url
+        }
+        return total
+    except:
+        return 'Account not found'
+    finally:
+        driver.quit()
+
+
+if __name__ == '__main__':
+    print(origin_login('apocolistm@gmail.com'))
