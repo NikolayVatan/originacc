0283c8219214132ef8ba49d9d14c5687f074213b
NikolayVatan <51362503+NikolayVatan@users.noreply.github.com>
2019-10-21 11:32:04 +0300

    Update originacc.py

--- originacc.py
+++ originacc.py
@@ -12,10 +12,8 @@ def origin_login(email):
     driver.set_window_size(1280, 768)
     wait = WebDriverWait(driver, 15)
     driver.get('https://www.origin.com')
-    wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="shell"]/section/div/nav/div/div[5]/ul/li[1]/'
-                                                           'origin-cta-login/origin-cta-primary')))
-    driver.find_element_by_xpath('//*[@id="shell"]/section/div/nav/div/div[5]/ul/li[1]/origin-cta-login/'
-                                 'origin-cta-primary').click()
+    wait.until(EC.visibility_of_element_located((By.XPATH, './/origin-cta-login//a')))
+    driver.find_element_by_xpath('.//origin-cta-login//a').click()
     login_window = driver.window_handles[1]
     login_window = driver.window_handles[1]
     driver.switch_to.window(login_window)
